5767bb592129cbe725931184da45754e655867ed
nakashimmer <nakacma@gmail.com>
2019-09-26 21:41:41 +0900

    up

---
+++ 2019/20190926/index/ga.js
@@ -0,0 +1,12 @@
+//google analytics
+document.write(
+	'<script async src="https://www.googletagmanager.com/gtag/js?id=UA-136606141-1"></script>'
+);
+
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+	dataLayer.push(arguments);
+}
+
+gtag('js', new Date()); gtag('config', 'UA-136606141-1');
